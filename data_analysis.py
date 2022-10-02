598885ca18d4a993846c3b8972467c23a66f8060
prateek <prateek@cloudcycle.com>
2022-10-02 16:02:10 +0100

    using power transformation on asking price to normalize and then calculate growth rate

--- data_analysis.py
+++ data_analysis.py
@@ -3,6 +3,9 @@ import pandas as pd
 import holoviews as hv
 from IPython.display import display
 from sklearn.linear_model import LinearRegression, RANSACRegressor
+from sklearn.preprocessing import PowerTransformer, QuantileTransformer, power_transform
+from sklearn.pipeline import make_pipeline
+from scipy import stats
 import logging
 logging.basicConfig(format="%(asctime)s %(levelname)s %(funcName)s : %(message)s")
 
@@ -45,27 +48,29 @@ def drop_bathroom_outliers(df_listing):
     return df
 
 
-def make_reg_plot(df_data, col_x, col_y):
+def make_reg_plot(df, col_x, col_y, outlier_threshold=2):
     """Make a regression line plot on passed columns and return plot along with coefficient and intercept of regression"""
-    df_data = df_data.copy().sort_values(col_x).reset_index(drop=True)
+    df_data = df.copy().sort_values(col_x).reset_index(drop=True)
     if col_x == 'listing_dt':
         X = df_data.get(col_x) - df_data.get(col_x).iloc[0]
         X = X.dt.days.values.reshape(-1,1)
     else:
         X = df_data.get(col_x).reshape(-1,1)
         
-    y = df_data.get(col_y)
+    y = df_data.get(col_y).values
+    # filter_index = np.where((y>=-outlier_threshold) & (y<=outlier_threshold))[0]
+    # X,y = X[filter_index], y[filter_index]
     model = LinearRegression()
     model.fit(X,y)
     y_pred = model.predict(X)
-    X_plot = df_data.sort_values(col_x).get(col_x)
+    X_plot = df_data.get(col_x)
     sc = hv.Curve((X_plot, y_pred))
-    print(model.score(X,y))
+    print("r2_score:", model.score(X,y))
     return sc, model.coef_, model.intercept_
 
 
-def plot_daily_price(df, agg_func=np.mean, grouping_col='property_type'):
-    """Plot daily price aggregated daily by agg_func, plot sparately for each value in grouping_col"""
+def plot_daily_price(df, agg_func=np.mean, transform_func=lambda x:x, grouping_col='property_type'):
+    """Plot daily price aggregated daily by agg_func, then transforming aggregated asking_price by transform_func and finally plot separately for each value in grouping_col"""
     agg = (df
            .get(['listing_dt', grouping_col, 'asking_price'])
            .groupby([grouping_col, 'listing_dt'])
@@ -74,19 +79,52 @@ def plot_daily_price(df, agg_func=np.mean, grouping_col='property_type'):
            .reset_index()
           )
 
-    o_ = (hv.Scatter(hv.Dataset(agg, kdims=['listing_dt', grouping_col], vdims='asking_price'))
-          .groupby(grouping_col)
-          .overlay()
-          .opts(hv.opts.Scatter(alpha=0.5, width=600, height=350, show_grid=True, tools=['hover']))
-         )
-
-    dist = []
+    overlay_list = []
     for group, df_group in agg.groupby(grouping_col):
-        dist.append(hv.Distribution(df_group, 'asking_price'))
-    
-    overlay = o_ << hv.Overlay(dist).opts(show_grid=True, width=150, height=350)
+        df_group['asking_price'] = transform_func(df_group['asking_price'].values.reshape(-1,1))
+        scatter = hv.Scatter(hv.Dataset(df_group, 'listing_dt', 'asking_price', label=group))
+        dist = hv.Distribution(df_group, 'asking_price')
+        overlay_list.append(scatter << dist)
+
+    overlay = (hv.Overlay(overlay_list)
+               .collate()
+               .opts(hv.opts.Scatter(alpha=0.5, width=600, height=350, 
+                                     show_grid=True, tools=['hover']))
+              )
+
     return overlay
 
+
+def growth_days(df_coef, days=365*2):
+    """Estimate growth over numer of days using coefficients from box-cox transformation and line fit"""
+    numerator = df_coef['normalize_lambda']*df_coef['normalized_std']*df_coef['coef']*days
+    denominator = (df_coef['normalize_lambda']*df_coef['normalized_std']*(0 + df_coef['intercept']) 
+                   + 1 + df_coef['normalized_mean']*df_coef['normalize_lambda']
+                  )
+    return (1 + numerator/denominator)**(1/df_coef['normalize_lambda'])
+
+
+def inverse_transform(yt, df_coef):
+    """Inverse transform from power domain to price domain for sanity check on data. 
+    Assumption is that Power transform uses Box-Cox transformation yt = (y**l - 1)/l and then scaling transformed values by mean and std"""
+    y = (yt*df_coef['normalized_std']*df_coef['normalize_lambda']
+         + 1
+         + df_coef['normalized_mean'] * df_coef['normalize_lambda']
+        )**(1/df_coef['normalize_lambda'])
+
+    return y
+    
+
+def qqplot(data):
+    """Plot qq-plot for data to check normality"""
+    x,y = stats.probplot(data)[0]
+    
+    scatter = hv.Scatter((x,y)).opts(width=400, height=350, show_grid=True, xlabel='Theoretical quantiles', ylabel='Sample quantiles')
+    line = hv.Curve(zip([np.min(x), np.max(x)], [np.min(y), np.max(y)])).opts(color='red')
+    
+    overlay = scatter*line
+    return overlay.opts(title='Q-Q Plot')
+    
 
 if __name__ == '__main__':
     
@@ -158,7 +196,7 @@ if __name__ == '__main__':
     
     grouping_col = 'property_type'
     for location, df_location in df_listing.groupby('location'):
-        overlay = plot_daily_price(df_location, np.mean, grouping_col)
+        overlay = plot_daily_price(df_location, agg_func=np.median, grouping_col=grouping_col)
         display(overlay.opts(hv.opts.Scatter(title=location)))
     
     # asking_price ranges from 100000 to 19000000, seems like capped at lower end specially for Wolverhampton
@@ -195,31 +233,52 @@ if __name__ == '__main__':
     # Which locations have experienced the fastest growth in asking prices over the period ?
     
     # We can group by date to remove any variation during the day since we're interested in longer term behavior
-
-    coef_list = []
+    # We will split the data into house and flat separately to assess growth rates
+    
+    coef_list = [] 
     grouping_col = 'property_type'
     for location, df_location in df_listing.groupby('location'):
-        overlay = plot_daily_price(df_location, lambda x: np.log10(np.mean(x)), grouping_col)
+        # First we plot prices in a transformed domain where they are approximately normal
+        overlay = plot_daily_price(df_location, 
+                                   agg_func=np.median, 
+                                   transform_func=lambda x: power_transform(x, method='box-cox', standardize=True), 
+                                   grouping_col=grouping_col)
         
         regs = []
-        for group, df_group in df_location.groupby(grouping_col):
-            reg_, coef, intercept = make_reg_plot(df_group.assign(log10_asking_price=lambda x: np.log10(x['asking_price'])), 
-                                                  'listing_dt', 
-                                                  'log10_asking_price')
+        df_date_reduced = (df_location
+                           .groupby([grouping_col, 'listing_dt'])
+                           .median()
+                           .reset_index()
+                          )
+        for group, df_group in df_date_reduced.groupby(grouping_col):
+            # For each group that we're interested in fit a line to data and get coefficients
+            normalize = PowerTransformer(method='box-cox', standardize=True).fit(df_group['asking_price'].values.reshape(-1,1))
+            
+            y_org = df_group['asking_price'].values.reshape(-1,1)
+            y_transformed = (y_org**(normalize.lambdas_[0]) -1)/normalize.lambdas_[0]
+            
+            df_group['target'] = normalize.transform(df_group['asking_price'].values.reshape(-1,1))
+
+            reg_, coef, intercept = make_reg_plot(df_group, 'listing_dt', 'target')
             display(f"{group} - Slope: {coef[0]}, Intercept: {round(intercept,2)}")
             
             coef_list.append({'location': location, 'group': group,
-                             'coef': coef[0], 'intercept': intercept})
+                             'coef': coef[0], 'intercept': intercept, 
+                              'normalize_lambda': normalize.lambdas_[0],
+                             'normalized_mean': np.mean(y_transformed),
+                             'normalized_std': np.std(y_transformed)})
             regs.append(reg_)
+            display(qqplot(df_group['target'].values))
             
-        o = overlay.opts(hv.opts.Scatter(title=location, ylabel='log10 (asking_price)'))*hv.Overlay(regs)
+        o = overlay.opts(hv.opts.Scatter(title=location, ylabel='Transformed asking_price'))*hv.Overlay(regs)
         display(o)
         
     
+    # Estimating Growth rate from line fit 
     df_coef = (pd.
                DataFrame(coef_list)
-               .assign(growth_2_yr = lambda x: 10**(x['coef']*2*365))
-               .sort_values(['group', 'growth_2_yr'], ascending=False)
+               .assign(growth_975_days = lambda x: growth_days(x, days=(365*2 + 8*30)))
+               .sort_values(['group', 'growth_975_days'], ascending=False)
               )
 
     display(df_coef)
@@ -228,19 +287,42 @@ if __name__ == '__main__':
     # colormap = {'freehold':'#377eb8', 'leasehold':'#e41a1c', 'unknown':'984ea3'}
     for group, df_group in df_coef.groupby('group'):
         o = (hv
-             .Bars(df_group, 'location', 'growth_2_yr', label=group.capitalize())
+             .Bars(df_group, 'location', 'growth_975_days', label=group.capitalize())
              .opts(width=600, height=350, 
                    xrotation=90, tools=['hover'], 
                    color=colormap[group], alpha=0.5, show_legend=False,
-                   ylabel='Growth over 2 yrs')
+                   ylabel='Growth (over 975 days)')
             )
         display(o*hv.HLine(1).opts(color='black', line_dash='dashed'))
     
     
+    # ------------- Sanity check on calulated growth values ---------- # 
+    list_res = []
+    for i, row in df_coef.iterrows():
+        yt = np.array([row['intercept'], row['coef']*975+row['intercept']])
+        y = inverse_transform(yt, row)
+        res = {'lower_pred': y[0], 'upper_pred':y[1], 'group':row.group, 
+               'location':row.location, 'growth_pred': row.growth_975_days}
+        df_location = df_listing.query("location==@row.location")
+        df_group = (df_location
+                    .groupby(['property_type', 'listing_dt'])
+                    .median()
+                    .reset_index()
+                    .query("property_type == @row.group")
+                    .sort_values('listing_dt')
+                   )
     
+        res['lower_act'] = df_group.iloc[:5]['asking_price'].median()
+        res['upper_act'] = df_group.iloc[-5:]['asking_price'].median()
+        res['growth_act'] = res['upper_act']/res['lower_act']
+        
+        list_res.append(res)
+        
+    df_res = pd.DataFrame(list_res)
     
-    
-    
+    display(hv.Scatter(df_res, 'lower_act', 'lower_pred').opts(show_grid=True, height=350, width=400, xrotation=90))
+    display(hv.Scatter(df_res, 'upper_act', 'upper_pred').opts(show_grid=True, height=350, width=400, xrotation=90))
+    display(hv.Scatter(df_res, 'growth_act', 'growth_pred').opts(show_grid=True, height=350, width=400, xrotation=90))
     
     
     
